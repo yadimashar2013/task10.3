7c0afa962a843a611457059f3ae8165cb81bf0fc
yadimashar2013 <162845121+yadimashar2013@users.noreply.github.com>
2024-06-15 14:16:46 +0700

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,43 @@
+import threading
+
+
+class BankAccount:
+    def __init__(self):
+        self.balance = 1000
+        self.lock = threading.Lock()
+
+    def deposit(self, amount):
+        with self.lock:
+            self.balance += amount
+
+            print(f"Deposited  {amount}, new balance is  {self.balance}")
+
+    def withdraw(self, amount):
+        with self.lock:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f"Withdrew {amount}, new balance is {self.balance}")
+            else:
+                print("Insufficient funds")
+
+
+def deposit_task(account, amount):
+    for _ in range(5):
+        account.deposit(amount)
+
+
+def withdraw_task(account, amount):
+    for _ in range(5):
+        account.withdraw(amount)
+
+
+account = BankAccount()
+
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
